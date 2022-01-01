cf4318057000db45c68375d9e0195ae1b9d86a8b
SaidAoussar <aoussarsaid33@gmail.com>
2022-01-01 15:05:33 +0100

    add createBlog,removeBlog,updateBlog fun to blog controller

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -17,25 +17,55 @@ const getOneBlog = async (req,res) => {
         const doc = await Blog.findById(req.params.id).exec()
         res.status(200).json(doc)
     } catch (e) {
-    console.log(e)
+        console.log(e)
+    }
+}
+
+const createBlog = async (req,res)=>{
+    try {
+        const doc = await Blog.create({
+            title: req.body.title,
+            body: req.body.body,
+            postTime: Date.now(),
+            tags: req.body.tags,
+            author: req.user._id
+        })
+        res.status(200).json(doc)
+    } catch (e) {
+        res.status(400).json(e)
+    }
+}
+
+const removeBlog = async (req,res)=>{
+    try {
+        const doc = await Blog.findOneAndRemove({
+            _id: req.params.id,
+            author: req.user._id
+        })
+        res.status(200).json(doc)
+    } catch (e) {
+       res.status(400).json(e) 
+    }
+}
+const updateBlog = async (req,res)=>{
+    try {
+        const doc = await Blog.findOneAndUpdate({
+            _id: req.body._id,
+            author: req.user._id
+        },
+        req.body,
+        { new: true })
+
+        res.status(200).json(doc)
+        
+    } catch (e) {
+        res.status(400).json(e)
     }
 }
 
 
-const createBlog = async (req,res) =>{}
-
-
-
-
-
-
-
-
-
-const constroller = {
-    getAllBlogs: getAllBlogs,
-    getOneBlog: getOneBlog
-}
-
-
-module.exports = constroller
+module.exports.getAllBlogs = getAllBlogs
+module.exports.getOneBlog = getOneBlog
+module.exports.createBlog = createBlog
+module.exports.removeBlog = removeBlog
+module.exports.updateBlog = updateBlog
