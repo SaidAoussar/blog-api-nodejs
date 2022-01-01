74968e0402b0e08d3f6d7ffda0280e2c1cb83e48
SaidAoussar <aoussarsaid33@gmail.com>
2022-01-01 15:07:32 +0100

    add some new routers to blog.router

--- src/routers/blog.router.js
+++ src/routers/blog.router.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const controller = require("../controllers/blog.controller")
+const { getAllBlogs, getOneBlog, createBlog, removeBlog, updateBlog } = require("../controllers/blog.controller")
 const verify = require("../utils/verify_token")
 
 
-router.get('/',verify,controller.getAllBlogs)
-router.get('/:id',controller.getOneBlog)
+router.get('/',verify,getAllBlogs)
+router.get('/:id',getOneBlog)
+router.post('/',createBlog)
+router.delete('/:id',removeBlog)
+router.put('/',updateBlog)
 
 
 
