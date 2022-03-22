982bb9cc436468b98609121e941f1bc9e20042de
SaidAoussar <aoussarsaid33@gmail.com>
2022-03-22 12:39:25 +0100

    implement pagination in getAllBlogs

--- src/routers/blog.router.js
+++ src/routers/blog.router.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const { getAllBlogs, getOneBlog, createBlog, removeBlog, updateBlog,allBlogsOfUser } = require("../controllers/blog.controller")
-const verify = require("../utils/verify_token")
+const express = require("express");
+const router = express.Router();
+const {
+  getAllBlogs,
+  getOneBlog,
+  createBlog,
+  removeBlog,
+  updateBlog,
+  allBlogsOfUser,
+} = require("../controllers/blog.controller");
+const { paginatedResults } = require("../utils/pagination");
+const verify = require("../utils/verify_token");
+const Blog = require("../models/blog.model");
 
+router.get("/", getAllBlogs);
+// router.get("/", paginatedResults(Blog), getAllBlogs);
+router.get("/:id", getOneBlog);
+router.post("/", verify, createBlog);
+router.delete("/:id", verify, removeBlog);
+router.put("/", verify, updateBlog);
+router.get("/user/:id", allBlogsOfUser);
 
-router.get('/',getAllBlogs)
-router.get('/:id',getOneBlog)
-router.post('/',createBlog)
-router.delete('/:id',removeBlog)
-router.put('/',updateBlog)
-router.get('/user/:id',allBlogsOfUser)
-
-
-
-module.exports = router
+module.exports = router;
