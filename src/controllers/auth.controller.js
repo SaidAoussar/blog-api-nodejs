ce4d277754eeb53e309d49470bbb84495e4bedb0
SaidAoussar <aoussarsaid33@gmail.com>
2022-03-22 12:39:01 +0100

    change formating of data send in login when auth success

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,67 +1,78 @@
-const User = require('../models/user.model')
-const { registerValidation, loginValidation } = require('../utils/validation')
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const User = require("../models/user.model");
+const { registerValidation, loginValidation } = require("../utils/validation");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const register = async (req,res)=>{
+const register = async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).json({ message: error.details[0].message });
 
-    const {error} = registerValidation(req.body);
-    if(error) return res.status(400).json({message: error.details[0].message})
+  //hash password
 
-    //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(req.body.password,salt)
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashPassword,
+  });
 
-    const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashPassword
-    })
+  try {
+    const savedUser = await user.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
-    try {
-        const savedUser =  await user.save()
-        res.status(200).json(savedUser)
-    } catch (err) {
-       res.status(400).json(err) 
-    }
-}
+const login = async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).json({ message: error.details[0].message });
 
-const login =  async (req,res) => {
-    const {error} = loginValidation(req.body);
-    if(error) return res.status(400).json({message: error.details[0].message})
+  //check email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json({ message: "Email not exist" });
 
-    //check email
-    const user = await User.findOne({email: req.body.email})
-    if(!user) return res.status(400).json({message: 'Email not exist'})
+  //check password
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).json({ message: "invalid password" });
 
-    //check password
-    const validPass = await bcrypt.compare(req.body.password,user.password)
-    if(!validPass) return res.status(400).json({message: "invalid password"})
+  //token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).status(200).json({
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+    token: token,
+  });
+  /* blog 2
+  res
+    .header("auth-token", token)
+    .status(200)
+    .json({
+      token: token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+    */
+};
 
-    //token
-    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token',token).status(200).json({"token": token,"user":{
-        "_id": user._id,
-        "username": user.username,
-        "email": user.email
-    }})
-}
+const isAuthenticated = async (req, res) => {
+  try {
+    const doc = await User.findById(req.user._id, ["email", "username"]);
+    res.status(200).json({
+      ...doc._doc,
+      isAuth: true,
+    });
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
 
-const isAuthenticated = async (req,res) => {
-    try {
-        const doc = await User.findById(req.user._id,["email","username"])
-        res.status(200).json({
-            ...doc._doc,
-            isAuth : true
-        })
-     } catch (e) {
-         res.status(400).json(e)
-     }
-}
-
-
-
-module.exports.register = register
-module.exports.login = login
-module.exports.isAuthenticated = isAuthenticated
+module.exports.register = register;
+module.exports.login = login;
+module.exports.isAuthenticated = isAuthenticated;
