d3ab5326e4c7fbdc2f3c8c43b9bc3129eb0e47de
SaidAoussar <aoussarsaid33@gmail.com>
2022-03-22 12:40:11 +0100

    just delete verify from getAllUsers route

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,14 +1,16 @@
-const express = require("express")
-const {getAllUsers, getUser, removeUser, updateUser} = require("../controllers/user.controller")
-const router = express.Router()
-const verify = require("../utils/verify_token")
+const express = require("express");
+const {
+  getAllUsers,
+  getUser,
+  removeUser,
+  updateUser,
+} = require("../controllers/user.controller");
+const router = express.Router();
+const verify = require("../utils/verify_token");
 
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.delete("/:id", removeUser);
+router.put("/:id", verify, updateUser);
 
-router.get("/",verify,getAllUsers)
-router.get("/:id",getUser)
-router.delete("/:id", removeUser)
-router.put("/:id",verify,updateUser)
-
-
-
-module.exports = router
+module.exports = router;
